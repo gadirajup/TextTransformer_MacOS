efc5cbdbf1ee006046646dcf4270c98f8d608ce0
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-14 12:43:29 -0700

    - Added to Status Bar

--- TextTransformer/AppDelegate.swift
+++ TextTransformer/AppDelegate.swift
@@ -11,16 +11,28 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "🔥"
+        statusItem.button?.target = self
+        statusItem.button?.action = #selector(showSettings)
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func showSettings() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") as? ViewController else {
+            fatalError("Unable to find ViewController in the storyboard")
+        }
+        
+        let popOverView = NSPopover()
+        popOverView.contentViewController = vc
+        popOverView.behavior = .transient
+        popOverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY)
+    }
 }
-
