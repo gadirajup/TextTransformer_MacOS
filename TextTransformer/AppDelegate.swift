122926cd8368822c4cbc24607feb3befb9a5884b
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-14 11:58:26 -0700

    Initial Commit

---
+++ TextTransformer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextTransformer
+//
+//  Created by Prudhvi Gadiraju on 4/14/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
