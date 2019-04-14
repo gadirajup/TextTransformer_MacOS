147587d1af6242e0a9e31477b8607a3f35422046
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-14 12:26:46 -0700

    - Connected Outlets - Logic Functions

--- TextTransformer/ViewController.swift
+++ TextTransformer/ViewController.swift
@@ -8,12 +8,16 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTextFieldDelegate {
 
+    @IBOutlet weak var input: NSTextField!
+    @IBOutlet weak var type: NSSegmentedControl!
+    @IBOutlet weak var output: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        typeChange(self)
     }
 
     override var representedObject: Any? {
@@ -22,6 +26,41 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func typeChange(_ sender: Any) {
+        switch type.selectedSegment {
+        case 0:
+            output.stringValue = rot13(input.stringValue)
+        case 1:
+            output.stringValue = similar(input.stringValue)
+        case 2:
+            output.stringValue = strike(input.stringValue)
+        default:
+            output.stringValue = zalgo(input.stringValue)
+        }
+    }
+    
+    @IBAction func copyToPasteboard(_ sender: Any) {
+    }
+    
+    func controlTextDidChange(_ obj: Notification) {
+        typeChange(self)
+    }
+    
+    func rot13(_ input: String) -> String {
+        return "ROT13: " + input
+    }
+    
+    func similar(_ input: String) -> String {
+        return "similar: " + input
+    }
+    
+    func strike(_ input: String) -> String {
+        return "strike: " + input
+    }
+    
+    func zalgo(_ input: String) -> String {
+        return "zalgo: " + input
+    }
+    
 }
 
